4bf3ee0d3ab4bb75fce89d8b080c52d68dcf4f6d
Theo Hatzi <theo.hatzii@gmail.com>
2016-02-21 22:46:50 +1100

    Remove unused jsdom from elements

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -1,9 +1,5 @@
-import { jsdom } from 'jsdom';
 import chai from 'chai';
 import sinon from 'sinon';
 
-global.document = jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-global.navigator = global.window.navigator;
 global.expect = chai.expect;
 global.sinon = sinon;
